1d715ea2332fc5a965eb16c8d8c4a555ea637ce4
Nikolay56615 <Nikolay56615@users.noreply.github.com>
2024-09-16 06:15:06 +0000

    Deploying to gh-pages from @ Nikolay56615/OOP@fcea98d814b3de944fd189e3c45afd3d334bc194 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.lebedev","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.lebedev","l":"Card"},{"p":"ru.nsu.lebedev","l":"Deck"},{"p":"ru.nsu.lebedev","l":"Main"}]
