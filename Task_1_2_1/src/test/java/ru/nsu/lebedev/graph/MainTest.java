b44bac1e6d3acebfdab30b56681419c3c041759b
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2024-10-13 18:14:19 +0700

    test

---
+++ Task_1_2_1/src/test/java/ru/nsu/lebedev/graph/MainTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.lebedev.graph;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void test() {
+        assertEquals(5, Main.sum(2, 3));
+    }
+
+}
