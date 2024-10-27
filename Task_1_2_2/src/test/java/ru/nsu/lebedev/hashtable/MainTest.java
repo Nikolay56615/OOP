9738631238f6ea7bc2024224e0edb4ba14642084
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2024-10-27 14:38:00 +0700

    initial commit

---
+++ Task_1_2_2/src/test/java/ru/nsu/lebedev/hashtable/MainTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.lebedev.hashtable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() {
+        assertEquals(5, Main.sum(5, 0));
+    }
+}
