cc9f2c57dda8edd6916ad78358d2a2be88b6de2e
Nikolay56615 <75627798+Nikolay56615@users.noreply.github.com>
2025-03-07 22:57:32 +0700

    test Coverage

--- Task_2_2_1/src/test/java/ru/nsu/lebedev/pizzeria/MainTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/lebedev/pizzeria/MainTest.java
@@ -1,7 +1,14 @@
 package ru.nsu.lebedev.pizzeria;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 class MainTest {
-
+    @Test
+    void callMain() throws IOException {
+        Main.main(new String[]{});
+        assertTrue(true);
+    }
 }
